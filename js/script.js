16cf89e96bffc86cb37600d5294649d37735a5ce
marthayulinda <marthayulindatobing@gmail.com>
2024-07-31 17:42:35 +0700

    coba lagi

--- js/script.js
+++ js/script.js
@@ -7,19 +7,51 @@ document.addEventListener("DOMContentLoaded", function () {
       var email = document.getElementById("email").value;
       var ask = document.getElementById("ask").value;
 
+      // Clear previous error messages
+      clearErrors();
+
+      var isValid = true;
+
       if (name === "") {
-        alert("Name must be filled out");
-        event.preventDefault();
-      } else if (email === "") {
-        alert("Email must be filled out");
-        event.preventDefault();
-      } else if (ask === "") {
-        alert("Please select an option");
+        showError("name", "*name must be filled out");
+        isValid = false;
+      }
+      if (email === "") {
+        showError("email", "*email must be filled out");
+        isValid = false;
+      }
+      if (ask === "") {
+        showError("ask", "*please select an option");
+        isValid = false;
+      }
+
+      if (!isValid) {
         event.preventDefault();
       } else {
         alert("Form submitted successfully!");
       }
     });
+
+  // Clear previous error messages
+  function clearErrors() {
+    var errorMessages = document.querySelectorAll(".error-message");
+    errorMessages.forEach(function (msg) {
+      msg.remove();
+    });
+  }
+
+  // Show error message
+  function showError(inputId, message) {
+    var inputElement = document.getElementById(inputId);
+    var errorMessage = document.createElement("div");
+    errorMessage.className = "error-message";
+    errorMessage.style.color = "red";
+    errorMessage.innerText = message;
+    inputElement.parentNode.insertBefore(
+      errorMessage,
+      inputElement.nextSibling
+    );
+  }
 
   // Smooth scrollnavigasi
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
@@ -45,7 +77,25 @@ document.addEventListener("DOMContentLoaded", function () {
       firstIndex = 1;
     }
     img[firstIndex - 1].style.display = "block";
-    setTimeout(automaticSlide, 2000); // Move the setTimeout inside the function
+    setTimeout(automaticSlide, 2000);
   }
   automaticSlide();
+});
+
+//bold nav
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("main > section, main > article");
+  const navLinks = document.querySelectorAll(".nav-item");
+
+  function changeActiveNav() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    navLinks[index].classList.add("active");
+  }
+
+  changeActiveNav();
+  window.addEventListener("scroll", changeActiveNav);
 });
